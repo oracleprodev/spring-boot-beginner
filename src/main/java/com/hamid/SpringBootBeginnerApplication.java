0dede5d50d18c5632541bb8b87052aa126a1a357
Hamid Reza <oracleprodev@gmail.com>
2024-11-29 20:21:26 +0330

    Initialize Project

---
+++ src/main/java/com/hamid/SpringBootBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.hamid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBeginnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamid/SpringBootBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeginnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
